de19972b1de587fa08b4bcd2c4b657c05e9d52ea
omodigital <omodigitallabs@gmail.com>
2025-04-13 01:34:48 +0530

    commit 3

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "👉 Building Vite project..."
-npm run build
-
-echo "🚀 Uploading dist/ to VPS..."
-rsync -avz --delete dist/ root@talkify.omodigital.io:/home/talkify/htdocs/dist/
-
-echo "✅ Deployed successfully to VPS!"
